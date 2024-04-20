51bf5e227da6e45648618f47bf9ed4462d1f23f4
Shaylavovsh <topovayaladaiada@gmail.com>
2024-04-20 17:16:43 +0500

    ASD

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,22 @@ import Settings from "../pages/Settings";
 
 const Navbar = () => {
   return (
-    <div>
-      <nav className="container py-[18px] border-b-[#D5D5D5] border-b ">
-        <div className="flex justify-between max-w-[95%] mx-auto">
-        <div className="flex gap-[22px] items-center justify-center">
-          <img src="public/carbon_logo-flickr.png" alt="" />
-          <img src="public/Admin Logo.png" alt="" />
-        </div>
-        
-        <div className="flex items-center gap-[33px]">
-        <Settings />
-          <img src="public/notifications.png" alt="" />
-          <div className="flex items-center gap-[15px]">
-            <p>Femi John</p>
-            <img src="public/Photo_7.png" alt="" />
+    <div className="w-full">
+      <nav className="py-[18px] border-b-[#D5D5D5] border-b w-full">
+        <div className="flex justify-between mx-auto container">
+          <div className="flex gap-[22px] items-center justify-center">
+            <img src="public/carbon_logo-flickr.png" alt="" />
+            <img src="public/Admin Logo.png" alt="" />
           </div>
-        </div>
+          
+          <div className="flex items-center gap-[33px]">
+          <Settings />
+            <img src="public/notifications.png" alt="" />
+            <div className="flex items-center gap-[15px]">
+              <p>Femi John</p>
+              <img src="public/Photo_7.png" alt="" />
+            </div>
+          </div>
         </div>
       </nav>
     </div>
